84a9456e4596880bc0110e53f53370ad7acf8a53
wildenali <wildeeeeen@gmail.com>
2019-03-07 09:00:34 +0700

    membuat class, dan membuat method dalam class basic

--- 24_Pengenalan_Class/class.py
+++ 24_Pengenalan_Class/class.py
@@ -1,5 +1,17 @@
+# class
+# class harus di atas, karena python itu interpreted language, makanya harus berurutan
 class mahasiswa():
-    nama = 'nama'
+    nama = 'nama'   # atribut, nilai yang nempel di dalam kelas
+
+    # membuat sebuah method
+    def belajar(self, kondisi):
+        print(self.nama, 'sedang belajar', kondisi)
+
+    def tidur(self):
+        print(self.nama, 'tidur di kelas')
+
+
+# main program
 
 otong = mahasiswa()
 ucup = mahasiswa()
@@ -13,3 +25,8 @@ otong.nama = "otong surotong"
 ucup.nama = "ucup suracup"
 print(otong.nama)
 print(ucup.nama)
+
+print()
+
+otong.belajar('dengan giat')
+ucup.tidur()
