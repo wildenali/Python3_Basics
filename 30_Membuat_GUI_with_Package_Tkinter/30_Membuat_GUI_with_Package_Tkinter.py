7295237cc7dbe935fd8426813eb32c1f63631359
wildenali <wildeeeeen@gmail.com>
2019-03-07 22:49:34 +0700

    cara menggunakan built in package di python, misal tkinter dan sekalian membuat gui sederhana

---
+++ 30_Membuat_GUI_with_Package_Tkinter/30_Membuat_GUI_with_Package_Tkinter.py
@@ -0,0 +1,16 @@
+# tkinter adalah salah satu GUI untuk python, yang built in
+
+import tkinter
+
+main_window = tkinter.Tk()
+
+def event_tekan():
+    label2 = tkinter.Label(main_window, text="aku diTEKAN")
+    label2.pack()
+
+label = tkinter.Label(main_window, text="halo, saya adalah \n GUI sederhana dengan \n menggunakan python")
+tombol = tkinter.Button(main_window, text="tekan aku", command = event_tekan)
+
+label.pack()
+tombol.pack()
+main_window.mainloop()
