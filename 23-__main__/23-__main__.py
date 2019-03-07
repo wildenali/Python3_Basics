b9294c2050e5b5ee79872073a138100e079f8547
wildenali <wildeeeeen@gmail.com>
2019-03-07 08:46:20 +0700

    cara menggunakan __main__

---
+++ 23-__main__/23-__main__.py
@@ -0,0 +1,9 @@
+import matematika as mm
+
+mm.tambah(3,4)
+mm.kurang(3,4)
+
+print(__name__)
+
+if __name__ == '__main__':
+    print('ini didalam main')
---
+++ 23-__main__/matematika.py
@@ -0,0 +1,13 @@
+def tambah(a, b):
+    print(a,'+',b,'=',a+b)
+
+def kurang(a, b):
+    print(a,'-',b,'=',a-b)
+
+# print(__name__)
+
+def main():
+    print('ini adalah fungsi utama dari matematik')
+
+if __name__ == '__main__':
+    main()
