1e9970d9f4cde87754c937b64184368e997c30f3
wildenali <wildeeeeen@gmail.com>
2019-03-07 08:50:12 +0700

    cara membuat class sederhana

---
+++ 24_Pengenalan_Class/class.py
@@ -0,0 +1,15 @@
+class mahasiswa():
+    nama = 'nama'
+
+otong = mahasiswa()
+ucup = mahasiswa()
+
+print(otong)
+print(ucup)
+
+print()
+
+otong.nama = "otong surotong"
+ucup.nama = "ucup suracup"
+print(otong.nama)
+print(ucup.nama)
