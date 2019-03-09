37763ad6b50b1d1e1702785a97e568fbe6f3d6ed
wildenali <wildeeeeen@gmail.com>
2019-03-09 16:35:56 +0700

    cara menggunkana virtual environment

---
+++ 32_Virtual_Environment/main.py
@@ -0,0 +1,10 @@
+# ini di buat di luar folder Python3_Basic
+# karena, klo di dalem folder itu, terlalu kepanjangan
+# jadi nanti muncul error, bad directory
+# solusinya bikin project nya di luar aja, setara sama Document foldernya
+
+import numpy as np
+
+a = np.array([1,2,3,4])
+b = np.array([5,6,7,9])
+print(a+b)
