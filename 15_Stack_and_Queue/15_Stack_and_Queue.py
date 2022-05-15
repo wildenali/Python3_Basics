40d8f3ede6ef7ef2663e808649cd98780a6d5e88
Wulan Panekenan <wulandaripanekenan@gmail.com>
2022-05-15 16:21:08 +0700

    stack and queue

--- 15_Stack_and_Queue/15_Stack_and_Queue.py
+++ 15_Stack_and_Queue/15_Stack_and_Queue.py
@@ -6,53 +6,53 @@
 # Stack - Contoh
 print('======= # Stack - Contoh =======')
 tumpukan = [1,2,3,4,5,6]
-print('data sekarang', tumpukan)
+print('data sekarang', tumpukan)        # data sekarang [1, 2, 3, 4, 5, 6]
 
 # memasukan data baru
 tumpukan.append(8)
-print('data masuk', 8)
+print('data masuk', 8)                  # data masuk 8
 tumpukan.append(9)
-print('data masuk', 9)
-print('data sekarang', tumpukan)
+print('data masuk', 9)                  # data masuk 9
+print('data sekarang', tumpukan)        # data sekarang [1, 2, 3, 4, 5, 6, 8, 9]
 
 # teknik stacking
 out = tumpukan.pop()  # menghilangkan data paling belakang
-print('data keluar', out)
-print('data sekarang', tumpukan)
+print('data keluar', out)               # data keluar 9
+print('data sekarang', tumpukan)        # data sekarang [1, 2, 3, 4, 5, 6, 8]
 
 
 # Queue - Contoh
 from collections import deque
 print('\n======= # Queue - Contoh =======')
 antrian = deque([1,2,3,4,5])      # tipe data deque
-print('data sekarang', antrian)
+print('data sekarang', antrian)         # data sekarang deque([1, 2, 3, 4, 5])
 
 # menambahkan data
 antrian.append(6)
-print('data masuk', 6)
-print('data sekarang', antrian)
+print('data masuk', 6)                  # data masuk 6
+print('data sekarang', antrian)         # data sekarang deque([1, 2, 3, 4, 5, 6])
 
 # menambahkan data
 antrian.append(7)
-print('data masuk', 7)
-print('data sekarang', antrian)
+print('data masuk', 7)                  # data masuk 7
+print('data sekarang', antrian)         # data sekarang deque([1, 2, 3, 4, 5, 6, 7])
 
 # mengurangi antrian
 out = antrian.popleft()
-print('data keluar', out)
-print('data sekrang', antrian)
+print('data keluar', out)               # data keluar 1
+print('data sekrang', antrian)          # data sekrang deque([2, 3, 4, 5, 6, 7])
 
 # mengurangi antrian
 out = antrian.popleft()
-print('data keluar', out)
-print('data sekrang', antrian)
+print('data keluar', out)               # data keluar 2
+print('data sekrang', antrian)          # data sekrang deque([3, 4, 5, 6, 7])
 
 # mengurangi antrian
 out = antrian.popleft()
-print('data keluar', out)
-print('data sekrang', antrian)
+print('data keluar', out)               # data keluar 3
+print('data sekrang', antrian)          # data sekrang deque([4, 5, 6, 7])
 
 # menambahkan data
 antrian.append(8)
-print('data masuk', 8)
-print('data sekarang', antrian)
+print('data masuk', 8)                  # data masuk 8
+print('data sekarang', antrian)         # data sekarang deque([4, 5, 6, 7, 8])
